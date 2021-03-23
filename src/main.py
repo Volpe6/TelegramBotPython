9f6a9e5e1deb9dc643a3241989378fe0238c493f
Volpe6 <baasch2014@gmail.com>
2021-03-23 18:34:37 -0300

    Muda a versão do numpy

--- src/main.py
+++ src/main.py
@@ -65,13 +65,13 @@ def main():
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     # Start the Bot
-    updater.start_polling()
+    # updater.start_polling()
 
     # # Start the Bot
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=int(PORT),
-    #                       url_path=TOKEN)
-    # updater.bot.setWebhook('https://mighty-basin-74579.herokuapp.com/' + TOKEN)
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://mighty-basin-74579.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
